901345c6dd33df0b9ff9f856a60496a583fbdef6
Dragos Sasaran <dragossasaran@192-168-0-104.rdsnet.ro>
2022-06-05 23:11:42 +0300

    minor changes

--- backend/server.js
+++ backend/server.js
@@ -52,6 +52,12 @@ if (process.env.NODE_ENV === "production") {
 app.use(notFound);
 app.use(errorHandler);
 
+const publicPath = path.join(__dirname, "..", "public");
+app.use(express.static(publicPath));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
